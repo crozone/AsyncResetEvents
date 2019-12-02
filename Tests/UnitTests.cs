0a2f8f0351e265a4aea0b6bd649174d81341baab
Ryan Crosby <ryan.crosby@live.com>
2019-12-02 14:43:05 +1100

    Add some basic unit tests

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -8,16 +8,45 @@ namespace Tests
     public class UnitTests
     {
         [Fact]
-        public void Test1()
+        public async Task BasicTest()
         {
             AsyncAutoResetEvent asyncAutoResetEvent = new AsyncAutoResetEvent();
 
-            Task awaitingTask = Task.Run(async () =>
+            Task triggerTask = Task.Run(async () =>
             {
-                await asyncAutoResetEvent.WaitAsync();
+                await Task.Delay(TimeSpan.FromSeconds(1));
+                asyncAutoResetEvent.Set();
             });
 
+            await asyncAutoResetEvent.WaitAsync();
+        }
 
+        [Fact]
+        public async Task BasicTestWithTimeout()
+        {
+            AsyncAutoResetEvent asyncAutoResetEvent = new AsyncAutoResetEvent();
+
+            Task triggerTask = Task.Run(async () =>
+            {
+                await Task.Delay(TimeSpan.FromSeconds(1));
+                asyncAutoResetEvent.Set();
+            });
+
+            Assert.True(await asyncAutoResetEvent.WaitAsync(TimeSpan.FromSeconds(5)));
+        }
+
+        [Fact]
+        public async Task BasicTestWithTimeoutTriggered()
+        {
+            AsyncAutoResetEvent asyncAutoResetEvent = new AsyncAutoResetEvent();
+
+            Task triggerTask = Task.Run(async () =>
+            {
+                await Task.Delay(TimeSpan.FromSeconds(5));
+                asyncAutoResetEvent.Set();
+            });
+
+            Assert.False(await asyncAutoResetEvent.WaitAsync(TimeSpan.FromSeconds(1)));
         }
     }
 }
